a6e7b1ba38a6ab80ce1ce727b34bc69836572f6e
Andrew-Chen-Wang <acwangpython@gmail.com>
2021-12-11 03:12:40 -0500

    Fix: got the extension popup appear * Background.js must save cache * I finally realized that background.js is actually long served. The caching mechanism can be saved via stringified JSON. Background.js now holds the cache and transfers it to the front-facing code. * Something I learned from the past was I couldn't get content_script and background.js to work cooperatively, so everything had to be injected via background.js. The same thing had to be done here, but I serve most of the code on initial utilization.

--- ios/Mobile Video Loop Extension/Resources/background.js
+++ ios/Mobile Video Loop Extension/Resources/background.js
@@ -1,5 +1,50 @@
+/**
+ * @typedef {Object} VideoCacheProperties
+ * @property {number} intervalMin lower bound of video for looping
+ * @property {number} intervalMax upper bound of video for looping
+ * @property {number} interval The actual, created interval to be cleared on "Cancel"
+ */
+/**
+ * @type {Object.<string,VideoCacheProperties>}
+ */
+let videoCache = {};
+
+function onExecuted(result) {
+    videoCache = result;
+    console.log(result);
+    console.log(videoCache);
+}
+
+function onError(error) {}
+
+function getCode() {
+    const stringified = JSON.stringify(videoCache);
+    // Contains the ShadowDOM overlay
+    return `
+function setupExtensionDiv() {
+    const extensionDiv = document.createElement("div");
+    document.body.append(extensionDiv);
+
+    let shadow = extensionDiv.attachShadow({mode: "open"});
+    return [extensionDiv, shadow];
+}
+shadowDomOverlay(JSON.parse('${stringified}'), ...setupExtensionDiv())
+`;
+}
+
+function executeExtensionCode() {
+    const executing = browser.tabs.executeScript({"code": getCode()});
+    executing.then(onExecuted, onError);
+}
+
+let hasInjectedOneTime = false;
 browser.browserAction.onClicked.addListener(
     function(tab) {
-        browser.tabs.sendMessage(tab.id, {"message": "hi"});
+        if (!hasInjectedOneTime) {
+            hasInjectedOneTime = true;
+            browser.tabs.executeScript({"file": "one-time.js"}, executeExtensionCode);
+        } else {
+            executeExtensionCode();
+        }
     }
 );
